54a2f2e2e54b2b4ecdf8f5109f1a8006b0ecbd0c
ishanRox <roxishan8@gmail.com>
2021-04-18 19:22:32 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-ivy-igqcebpipes
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-igqcebpipes)
---
+++ src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component, VERSION } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: [ './app.component.css' ]
+})
+export class AppComponent  {
+
+  onSubmit(form:NgForm){
+    console.log(form);
+  }
+}
