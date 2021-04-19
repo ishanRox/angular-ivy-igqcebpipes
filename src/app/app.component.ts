bfe66c1a51514c6209f97ce699f3c09648df683a
ishanRox <roxishan8@gmail.com>
2021-04-20 01:23:01 +0530

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,59 @@
-import { Component, VERSION } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import { Component, OnInit, VERSION } from "@angular/core";
+import { FormControl, FormGroup, NgForm, Validators } from "@angular/forms";
 
 @Component({
-  selector: 'my-app',
-  templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  selector: "my-app",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
-export class AppComponent  {
+export class AppComponent implements OnInit {
+  reactiveForm: FormGroup;
 
-  onSubmit(form:NgForm){
+  ngOnInit(): void {
+    this.reactiveForm = new FormGroup({
+      reactive_email: new FormControl("ishan@", [
+        Validators.required,
+        Validators.email,
+        //bind this if this class var used inside this validarot
+        this.forbiddenEmails.bind(this),
+     
+      ],
+       this.forbiddenEmailsAsync
+      )
+    });
+  }
+
+  onSubmit(form: NgForm) {
     console.log(form);
+    alert(JSON.stringify(form.value));
+  }
+
+  onSubmitReactive() {
+    alert(JSON.stringify(this.reactiveForm.value));
+  }
+
+  //custom  validator
+  forbiddenEmails(control: FormControl) {
+    if (["ishan@1", "ishan@rox"].includes(control.value)) {
+      //return error with true
+      return { "cant use that": true };
+    }
+    //null or valid if ok
+    return null;
+  }
+
+  //async validator
+  forbiddenEmailsAsync(control: FormControl) {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        if (["ishan@test"].includes(control.value)) {
+          //return error with true
+          resolve({ "cant use that": true });
+        } else {
+          //null or valid if ok
+          resolve(null);
+        }
+      }, 5000);
+    });
   }
 }
